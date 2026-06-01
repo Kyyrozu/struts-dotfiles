4bc59675e7b7e49e991d6584896a4618641faeb1
anthime <kyyrozupro@gmail.com>
2026-06-01 20:45:00 +0200

    NVIM: add live_grep to <space>fd

--- nvim/lua/anthime/plugins/telescope.lua
+++ nvim/lua/anthime/plugins/telescope.lua
@@ -6,27 +6,28 @@ return {
         -- optional but recommended
         { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
     },
-    config = function()
-      require('telescope').setup({
-	defaults = { 
-	    file_ignore_patterns = { 
-	      "node_modules",
-	      ".git/"
-	    }
-	  }
-      })
-      vim.keymap.set("n", "<space>fh", require('telescope.builtin').help_tags)
-      vim.keymap.set("n", "<space>ff", function() 
-	require('telescope.builtin').find_files {
-	  hidden = true
-	}
-      end)
-      vim.keymap.set("n", "<space>en", function()
-	require('telescope.builtin').find_files {
-	  hidden = true;
-	  cwd = vim.fn.stdpath("config");
-	}
-    end)
-  end
+	config = function()
+		require('telescope').setup({
+			defaults = {
+				file_ignore_patterns = {
+				  "node_modules",
+				  ".git/"
+				}
+			}
+		})
+		vim.keymap.set("n", "<space>fh", require('telescope.builtin').help_tags)
+		vim.keymap.set("n", "<space>ff", function()
+			require('telescope.builtin').find_files { hidden = true }
+		end)
+		vim.keymap.set("n", "<space>fg", function()
+			require('telescope.builtin').live_grep { hidden = true }
+		end)
+		vim.keymap.set("n", "<space>en", function()
+			require('telescope.builtin').find_files {
+				hidden = true;
+				cwd = vim.fn.stdpath("config");
+			}
+		end)
+	  end
   },
 }
