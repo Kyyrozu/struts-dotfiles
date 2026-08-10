120bd5b400b5ceca41733ae72c18c9226bd7b0d3
anthime <kyyrozupro@gmail.com>
2026-08-10 18:52:22 +0200

    various style cleaning

--- nvim/lua/anthime/init.lua
+++ nvim/lua/anthime/init.lua
@@ -6,3 +6,5 @@ require("anthime.lazy")
 vim.opt.shiftwidth = 4
 vim.opt.tabstop = 4
 vim.opt.number = true
+
+vim.opt.termguicolors = true
---
+++ nvim/lua/anthime/plugins/colorizer.lua
@@ -0,0 +1,10 @@
+return {
+  {
+    'norcalli/nvim-colorizer.lua',
+    version = '*',
+    config = function()
+	  require('colorizer').setup({
+      })
+    end,
+  },
+}
